5fe4401f399281ac88e71b95f79ae43fba974083
vedransehovic <vedran.sehovic@gmail.com>
2020-09-07 14:52:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
 
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
+  
+  namespace :admin do
+    resources :preferences
+  end
 
 end
